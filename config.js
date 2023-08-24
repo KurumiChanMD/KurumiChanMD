76d5a7a8c83e455af387ae318a7c493029cf7778
KurumiChanMD <143005964+KurumiChanMD@users.noreply.github.com>
2023-08-24 19:32:05 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,61 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+// Owner
+global.owner = [
+['6283846434821', 'Wen-XD', true]
+]
+global.mods = []
+global.prems = []
+// Info
+global.nomorwa = '6283846434821'
+global.packname = 'KurumiChan - MD by'
+global.author = '© Wen-XD'
+global.namebot = '​KurumiChan - MD'
+global.wm = '© KurumiChan By Wen-XD'
+global.stickpack = 'KurumiChan - MD by'
+global.stickauth = '© Wen-XD'
+global.fotonya = 'https://telegra.ph/file/83b522fd80b660705d1cd.jpg'
+// Link Sosmed
+global.sig = 'https://www.instagram.com/xyroinee'
+global.sgh = 'https://github.com/Xyroinee'
+global.sgc = 'https://chat.whatsapp.com/BJ4tTAzLTcR43stoLQGTPR'
+// Donasi
+global.psaweria = 'https://saweria.co/Zeltoria'
+global.ptrakterr = 'https://trakteer.id/zeltoria'
+global.povo = '0838-4643-4821'
+// Info Wait
+global.wait = 'Sedang Di Proses, Mohon Tunggu....'
+global.eror = 'Terjadi Kesalahan Coba Lagi Nanti!'
+global.multiplier = 69 
+// Apikey
+global.lol = 'apikeymu'
+global.rose = 'apikeymu'
+global.xyro = 'apikeymu'
+// Catatan : Jika Mau Work Fiturnya
+// Masukan Apikeymu
+// Gapunya Apikey? Ya Daftar
+// Website: https://api.xyroinee.xyz
+// Daftar Ke Website Tersebut Untuk
+// Mendapatkan Apikey Kamu
+global.APIs = {
+    xyro: "https://api.xyroinee.xyz",
+    popcat : 'https://api.popcat.xyz'
+}
+
+/Apikey/
+global.APIKeys = {
+    "https://api.xyroinee.xyz": "apikeymu",
+}
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
+})
+
